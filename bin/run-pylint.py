4ec51389d94637eec583139a92e31a94502d6174
sueastside <sueastside@gmail.com>
2013-11-02 11:30:25 +0100

    bah

--- bin/run-pylint.py
+++ bin/run-pylint.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     directory = os.path.dirname(os.path.abspath(__file__))
     config_file = os.path.join(directory, '..', 'pylint.cfg')
     directory = os.path.join(directory, '../damn_at')
-    lint.Run(list((directory, "--rcfile="+config_file, '--ignore=generated,b-script-*')), exit=False)
+    lint.Run(list((directory, "--rcfile="+config_file, '--ignore=generated,b-script-*')))
