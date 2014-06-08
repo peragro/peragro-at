93a358207704f327b81f2967ccd6b4e229be8b98
ayush1794 <ayush1794@gmail.com>
2014-06-08 22:57:17 +0000

    MetaDataEctractor Extended for images

--- damn_at/analyzers/image/analyzerimage.py
+++ damn_at/analyzers/image/analyzerimage.py
@@ -7,6 +7,7 @@ import mimetypes
 
 import subprocess
 
+from damn_at import MetaDataType, MetaDataValue
 from damn_at import FileId, FileDescription, AssetDescription, AssetId
 
 from damn_at.pluginmanager import IAnalyzer
@@ -44,7 +45,32 @@ class GenericImageAnalyzer(IAnalyzer):
             return False
             
         meta = {}
+        flag = 0
         lines = out.strip().split('\n')
+        for line in lines:
+            line = line.split(':', 1)
+            if len(line) == 1:
+                line = line[0].split('=')
+            line = [l.strip() for l in line]
+            if line[0] == 'MIME Type' : flag = 1
+            if flag == 1 and line[0] not in ['MIME Type', 'Image Size']:
+                meta[line[0].lower().replace(' ','_')] = line[1]
+
+        from damn_at.analyzers.image import metadata
+        
+        extractor_mapping = {'image/png' : metadata.MetaDataPNG,
+                'image/jpeg' : metadata.MetaDataJPG,
+                'image/x-ms-bmp' : metadata.MetaDataBMP,
+                'image/x-photoshop' : metadata.MetaDataPSD}
+        
+        if image_mimetype in extractor_mapping:
+            asset_descr.metadata = extractor_mapping[image_mimetype].extract(meta)
+        else:
+            asset_descr.metadata = {}
+
+        for key, value in meta.items():
+            if key not in asset_descr.metadata:
+                asset_descr.metadata['exif-'+key] = MetaDataValue(type=MetaDataType.STRING, string_value = value)
 
         file_descr.assets.append(asset_descr) 
         
