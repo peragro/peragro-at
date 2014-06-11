b221aa204efad1dcce695eb9e511a92b4669fa4e
ayush1794 <ayush1794@gmail.com>
2014-06-11 22:03:09 +0000

    Added mp32json support and some work on size problem

--- damn_at/transcoders/audio/wav2json/transcoderwav2json.py
+++ damn_at/transcoders/audio/wav2json/transcoderwav2json.py
@@ -1,5 +1,5 @@
 import os
-import wave, struct, json
+import wave, struct, json, mimetypes, subprocess
 
 from damn_at import logger
 from damn_at.transcoder import TranscoderException
@@ -9,7 +9,8 @@ from damn_at.options import IntOption, FloatOption, expand_path_template
 
 class Audio2ImageTranscoder(ITranscoder):
     options = [IntOption(name = 'channels', description = 'Number of channels to output', default = 2, min=1, max=2)]
-    convert_map = {"audio/x-wav" : {"application/json" : options}}
+    convert_map = {"audio/x-wav" : {"application/json" : options},
+            "audio/mpeg" : {"application/json" : options}}
 
     def __init__(self):
         ITranscoder.__init__(self)
@@ -22,12 +23,30 @@ class Audio2ImageTranscoder(ITranscoder):
 
         file_path = expand_path_template(target_mimetype.template,
                 target_mimetype.mimetype, asset_id, **options)
+        
+        audio_mimetype = mimetypes.guess_type(file_descr.file.filename)[0]
+        if audio_mimetype != "audio/x-wav":
+            try:
+                pro = subprocess.Popen(["sox", file_descr.file.filename, "-t", "wav", "tmp.wav"], 
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = pro.communicate()
+                if pro.returncode != 0:
+                    print("Sox failed %s with error code %d!" %(file_decrs.file.filename, pro.returncode), 
+                            out, err)
+                    return False
+                else:
+                    toopen = 'tmp.wav'
+            except OSError:
+                print("Sox failed %s!" %(file_descr.file.filename), out, err)
+                return False
+        else:
+            toopen = file_descr.file.filename
 
         try:
-            stream = wave.open(file_descr.file.filename, "rb")
+            stream = wave.open(toopen, "rb")
         except IOError:
-            print "cannot open", file_descr.file.filename
-            return false
+            print "cannot open", toopen
+            return False
         
         num_channels = stream.getnchannels()
         sample_width = stream.getsampwidth()
@@ -35,6 +54,7 @@ class Audio2ImageTranscoder(ITranscoder):
 
         raw_data = stream.readframes(num_frames)
         stream.close()
+        if toopen == 'tmp.wav': os.remove(toopen)
         
         total_samples = num_channels*num_frames
 
@@ -56,17 +76,15 @@ class Audio2ImageTranscoder(ITranscoder):
 
         if num_channels == options['channels']:
             if options['channels'] == 1:
-                transcoded = json.dumps({"mono": channels[0]}, indent=4, separators=(',', ': '))
+                transcoded = json.dumps({"mono": channels[0]})
             elif options['channels'] ==2:
-                transcoded = json.dumps({"left": channels[0], "right":
-                    channels[1]}, indent=4, separators=(',', ': '))
+                transcoded = json.dumps({"left": channels[0], "right": channels[1]})
 
         else:
             if options['channels'] == 1:
-                transcoded = json.dumps({"mono": channels[0]}, indent=4, separators=(',', ': '))
+                transcoded = json.dumps({"mono": channels[0]})
             else :
-                transcoded = json.dumps({"left": channels[0], "right":
-                    channels[0]}, indent=4, separators=(',', ': '))
+                transcoded = json.dumps({"left": channels[0], "right": channels[0]})
         
         full_path = os.path.join(dest_path, file_path)
         if not os.path.exists(os.path.dirname(full_path)):
