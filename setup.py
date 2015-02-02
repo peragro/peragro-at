4ef0d954f284b2561884a3d6f064b726aea53ede
Brant Watson <brant.watson@propylon.com>
2015-02-02 09:57:48 +0000

    Use normal setuptools package structure.  - Get version from git tags  - Start on RPM spec

--- setup.py
+++ setup.py
@@ -1,35 +1,48 @@
 import os
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import re
+import subprocess
+from setuptools import setup
+from setuptools import find_packages
 
-try:
-    assert 'damn-test-files' in os.listdir('..')
-except:
-    print "Please use git clone https://github.com/ayush1794/damn-test-files"
-    exit(1)
 
-config = {
-    'description': 'Digital Assets Managed Neatly: Analyzers and Transcoders',
-    'author': 'sueastside',
-    'url': 'https://github.com/sueastside/damn-at',
-    'download_url': 'https://github.com/sueastside/damn-at',
-    'author_email': 'No, thanks',
-    'version': '0.1',
-    'test_suite': 'tests.suite',
-    'install_requires': ['Yapsy', 'Image', 'pylint', 'thrift', 'gitpython',
-         'ffvideo', 'filemagic'], #'fusepy',
-    'packages': ['damn_at'],
-    'scripts': [],
-    'name': 'damn_at',
-    'entry_points':{
-          'console_scripts':['pt = damn_at.cli:main',
-                             'damn_at-server = damn_at.thrift.server:main',
-                             'damn_at-analyze = damn_at.analyzer:main',
-                             'damn_at-transcode = damn_at.transcoder:main',
-                             'damn_fs = damn_at.damnfs.damnfs:main']
+def version():
+    try:
+        pkg_info_version_re = re.compile("\nVersion: ([^\s]*)\n")
+        caller_path = os.path.dirname(__file__)
+        fd = open(os.path.join(caller_path, 'PKG-INFO'))
+        match = pkg_info_version_re.search(fd.read())
+        if match:
+            return match.groups()[0]
+        else:
+            raise Exception("No version match.")
+    except Exception:
+        args = ["git", "describe", "--all", "--always", "--dirty"]
+        try:
+            return subprocess.check_output(args).rstrip().lstrip("tags/")
+        except Exception:
+            return "0.0.0-development"
+
+INSTALL_REQUIRES = ['Yapsy', 'Image', 'gitpython', 'ffvideo', 'filemagic',
+                    'logilab-common']
+
+setup(
+    name='damn_at',
+    description='Digital Assets Managed Neatly: Analyzers and Transcoders',
+    packages=find_packages('src'),
+    package_dir={"": "src"},
+    version=version(),
+    author='sueastside',
+    url='https://github.com/sueastside/damn-at',
+    download_url='https://github.com/sueastside/damn-at',
+    author_email='No, thanks',
+    test_suite="tests",
+    install_requires=INSTALL_REQUIRES,
+    scripts=[],
+    entry_points={
+        'console_scripts': ['pt = damn_at.cli:main',
+                            'damn_at-server = damn_at.thrift.server:main',
+                            'damn_at-analyze = damn_at.analyzer:main',
+                            'damn_at-transcode = damn_at.transcoder:main',
+                            'damn_fs = damn_at.damnfs.damnfs:main']
     }
-}
-
-setup(**config)
+)
---
+++ src/damn_at/__init__.py
@@ -0,0 +1,38 @@
+"""
+The DAMN AT module.
+"""
+from __future__ import absolute_import
+import logging
+logger = logging.getLogger('damn_at')  # pylint: disable=C0103
+
+# Yapsy logs failed plugins as errors, but we don't like that.
+logging.getLogger('yapsy')._error = logging.getLogger('yapsy').error
+logging.getLogger('yapsy').error = logging.getLogger('yapsy').info
+
+
+from damn_at.serialization.generated.damn_types.ttypes import (
+    TargetMimetype,
+    TargetMimetypeOption,
+    MetaDataType,
+    MetaDataValue,
+    FileId,
+    FileDescription,
+    AssetId,
+    AssetDescription
+)
+
+from damn_at.analyzer import Analyzer  # pylint: disable=W0611
+from damn_at.metadatastore import MetaDataStore  # pylint: disable=W0611
+from damn_at.transcoder import Transcoder  # pylint: disable=W0611
+
+_CMD_DESCRIPTION = r'''
+ ___   _   __  __ _  _
+|   \ /_\ |  \/  | \| |
+| |) / _ \| |\/| | .` |
+|___/_/ \_\_|  |_|_|\_|
+    Digital Assets Managed Neatly.
+'''
+
+__all__ = ['TargetMimetype', 'TargetMimetypeOption', 'MetaDataType',
+           'MetaDataValue', 'FileId', 'FileDescription', 'AssetId',
+           'AssetDescription', 'Analyzer', 'MetaDataStore', 'Transcoder']
