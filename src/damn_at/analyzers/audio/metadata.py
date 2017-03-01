eab36ccd65e491906c2ad31d2f476138a2f2df94
Sambhav Kothari <sambhavs.email@gmail.com>
2017-03-01 15:12:00 +0530

    Add MetaDataAcoustID extractor class

--- src/damn_at/analyzers/audio/metadata.py
+++ src/damn_at/analyzers/audio/metadata.py
@@ -1,12 +1,18 @@
 from damn_at.metadata import MetaDataExtractor
 from damn_at import MetaDataType
-        
-    
+
+
 class MetaDataSox(MetaDataExtractor):
-    
+
     duration = MetaDataType.STRING, lambda context: context['duration']
     precision = MetaDataType.STRING, lambda context: context['precision']
     bit_rate = MetaDataType.STRING, lambda context: context['bit_rate']
     channels = MetaDataType.INT, lambda context: int(context['channels'])
     sample_rate = MetaDataType.INT, lambda context: int(context['sample_rate'])
+
+
+class MetaDataAcoustID(MetaDataExtractor):
+
+    duration = MetaDataType.STRING, lambda context: context['duration']
+    fingerprint = MetaDataType.STRING, lambda context: context['fingerprint']
 
