e6f5ecf189c197217a7b011a5612beb4f140fe2b
sagar-kohli <kohli.sagar2@gmail.com>
2017-03-26 02:43:15 +0530

    test_block_hashes_from_and_to_file

--- tests/test_bld.py
+++ tests/test_bld.py
@@ -15,7 +15,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(hashes, [])
 
     def test_block_hashes_for_file2(self):
-        """Test accurate hash generated from file"""
+        """Test accurate hashes generated from file"""
         data = (
             b'QlJBTlQgSVMgU1VQRVIgQVdFU09NRSBDT09MIEFORCBTSElULiBTTFVHUyBBUkUg'
             b'Q1VURQ=='
@@ -29,11 +29,16 @@ class TestCase(unittest.TestCase):
         self.assertEqual(digest, expected_digest)
         self.assertEqual(hashes, expected_hashes)
 
-    def test_block_hashes_from_file(self):
-        pass
+    def test_block_hashes_to_file_from_file(self):
+        """Test block_hashes_to_file and block_hashes_from_file"""
+        file_hash = 'cc163c1173bd99a6874d141cdc6b72ff1476e2e7'
+        block_hashes = ['4456fe0de4819f3ece3f6ed46ac02b15a3bfd086', '6179c639e0b4b021b75ac65d5fb4465ceb6c3157']
+        t = tempfile.NamedTemporaryFile(delete=False)
+        t.close()
+        bld.block_hashes_to_file(file_hash, block_hashes, t.name)
+        yield_hashes = list(bld.block_hashes_from_file(t.name))
+        self.assertEqual(yield_hashes, block_hashes)
 
-    def test_block_hashes_to_file(self):
-        pass
 
     def test_hash_to_dir(self):
         dir = bld.hash_to_dir('002badb952000339cdcf1b61a3205b221766bf49')
