0dab9deb96cd36408eb93fbf6dbce1abf7312173
sagar-kohli <kohli.sagar2@gmail.com>
2017-03-25 13:41:11 +0530

    some tests for bld.py

--- tests/test_bld.py
+++ tests/test_bld.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+import tempfile
 
 from mock import Mock, patch
 
@@ -9,7 +11,23 @@ class TestCase(unittest.TestCase):
     """Test Block level dedup"""
     def test_block_hashes_for_file(self):
         digest, hashes = bld.block_hashes_for_file('fakefile')
+        self.assertEqual(digest, 'NOT_FOUND(fakefile)')
+        self.assertEqual(hashes, [])
 
+    def test_block_hashes_for_file2(self):
+        """Test accurate hash generated from file"""
+        data = (
+            b'QlJBTlQgSVMgU1VQRVIgQVdFU09NRSBDT09MIEFORCBTSElULiBTTFVHUyBBUkUg'
+            b'Q1VURQ=='
+        )
+        expected_digest = '03439afa9d61f99f35d936b01ea8b4982ab247a0'
+        expected_hashes = ['03439afa9d61f99f35d936b01ea8b4982ab247a0']
+        t = tempfile.NamedTemporaryFile(delete=False)
+        t.write(data)
+        t.close()
+        digest, hashes = bld.block_hashes_for_file(t.name)
+        self.assertEqual(digest, expected_digest)
+        self.assertEqual(hashes, expected_hashes)
 
     def test_block_hashes_from_file(self):
         pass
@@ -29,3 +47,11 @@ class TestCase(unittest.TestCase):
 
     def test_walk(self):
         pass
+
+def test_suite():
+    """Return a list of tests"""
+    return unittest.TestLoader().loadTestsFromTestCase(TestCase)
+
+if __name__ == '__main__':
+    #unittest.main()
+    unittest.TextTestRunner().run(test_suite())
