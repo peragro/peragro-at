10a9a77bdfe6694a42089bba63fa9c8b2c8a714b
sagar-kohli <kohli.sagar2@gmail.com>
2017-05-30 14:25:20 +0530

    pep8 fix

--- src/damn_at/analyzers/audio/feature_extraction.py
+++ src/damn_at/analyzers/audio/feature_extraction.py
@@ -1,4 +1,5 @@
-'''Analyzer for audio feature extraction using Essentia'''
+"""Analyzer for audio feature extraction using Essentia"""
+
 from __future__ import print_function
 import os
 import mimetypes
@@ -17,7 +18,8 @@ def get_supported_formats():
         out, err = pro.communicate()
         if pro.returncode != 0:
             logger.debug(
-                "GetFeatureExtractorTypes failed with error code %d! ", pro.returncode,
+                "GetFeatureExtractorTypes failed with error code %d! ",
+                pro.returncode,
                 out,
                 err
             )
@@ -25,40 +27,56 @@ def get_supported_formats():
     except OSError as oserror:
         logger.debug("GetFeatureExtractorTypes failed! %s", oserror)
         return []
-    extensions = [line.split()[1] for line in out.decode("utf-8").split("\n")[4:] if len(line.split()) > 1]
+    extensions = [
+        line.split()[1] for line in out.decode("utf-8").split("\n")[4:]
+        if len(line.split()) > 1]
+
     mimes = []
     for ext in extensions:
         mime = mimetypes.guess_type('file.' + ext, False)[0]
         if mime and mime.startswith('audio/'):
             mimes.append(mime)
+
     return mimes
 
+
 def get_extracted_features(ofile):
-    '''returns the extracted features from json file in dictionary format'''
+    """returns the extracted features from json file in dictionary format"""
+
     features = {}
     with open(ofile, 'r') as ef:
         content = json.load(ef)
+
         if 'rhythm' in content:
             if 'bpm' in content['rhythm']:
                 features['bpm'] = content['rhythm']['bpm']
             if 'beats_count' in content['rhythm']:
                 features['beats_count'] = content['rhythm']['beats_count']
+
         if 'tonal' in content:
-            if 'chords_key' in content['tonal'] and 'chords_scale' in content['tonal']:
-                features['chord'] = str(content['tonal']['chords_key']) + ' ' + str(content['tonal']['chords_scale'])
-            if 'key_key' in content['tonal'] and 'key_scale' in content['tonal']:
-                features['key'] = str(content['tonal']['key_key']) + ' ' + str(content['tonal']['key_scale'])
+            if ('chords_key' in content['tonal']
+                    and 'chords_scale' in content['tonal']):
+                features['chord'] = \
+                    str(content['tonal']['chords_key']) + ' ' + \
+                    str(content['tonal']['chords_scale'])
+            if ('key_key' in content['tonal']
+                    and 'key_scale' in content['tonal']):
+                features['key'] = \
+                    str(content['tonal']['key_key']) + ' ' + \
+                    str(content['tonal']['key_scale'])
 
     return features
 
+
 class SoundAnalyzer(IAnalyzer):
-    '''Class for sound analyzer called in the analyzer'''
+    """Class for sound analyzer called in the analyzer"""
 
     handled_types = get_supported_formats()
 
     def __init__(self):
         IAnalyzer.__init__(self)
-        self.ex = os.path.join(os.path.dirname(__file__), 'extractors/streaming_extractor_music')
+        self.ex = os.path.join(os.path.dirname(__file__),
+                               'extractors/streaming_extractor_music')
 
     def activate(self):
         pass
@@ -68,9 +86,11 @@ class SoundAnalyzer(IAnalyzer):
         file_descr = FileDescription(file=fileid)
         file_descr.assets = []
 
-        asset_descr = AssetDescription(asset=AssetId(subname=os.path.basename(anURI),
-                                       mimetype=mimetypes.guess_type(anURI, False)[0],
-                                       file=fileid))
+        asset_descr = AssetDescription(asset=AssetId(
+            subname=os.path.basename(anURI),
+            mimetype=mimetypes.guess_type(anURI, False)[0],
+            file=fileid)
+        )
 
         output_file = os.path.abspath(anURI).split(".")[0] + ".json"
 
@@ -78,9 +98,12 @@ class SoundAnalyzer(IAnalyzer):
             if os.path.exists(self.ex):
                 subprocess.call([self.ex, anURI, output_file])
             else:
-                print("E: Extractor does not exist, please place the extractor at 'damn_at/analyzers/audio/extractors'")
+                print('E: Extractor does not exist, '
+                      'please place the extractor at '
+                      '\'damn_at/analyzers/audio/extractors\'')
         except Exception as e:
-            print(("E: Feature Extraction failed %s with error %s" % (anURI, e)))
+            print(('E: Feature Extraction failed %s with error %s'
+                   % (anURI, e)))
 
         meta = get_extracted_features(output_file)
         if os.path.exists(output_file):
