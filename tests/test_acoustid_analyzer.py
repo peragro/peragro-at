310814193af483456713a07f79e6d36def84effe
sagar-kohli <kohli.sagar2@gmail.com>
2017-07-10 06:55:19 +0530

    change

--- tests/test_acoustid_analyzer.py
+++ tests/test_acoustid_analyzer.py
@@ -46,8 +46,7 @@ class TestCase(unittest.TestCase):
                               "ChFCCBAMCQXAIQQIpBAwCDhAFBCecCAIo4wgAhAhgDiGiGE"
                               "CMCAEI-hgBQACxhEhAA'",
                'fingerprint_uuid': 'd6b5ffe5-a332-554c-8a68-56eae6f9fbfe',
-               'duration': '15.0044375s',
-               'hello_world': 'sagar-kohli'}
+               'duration': '15.0044375s'}
 
         analyzer = acoustid_analyzer.SoundAnalyzer()
         uri = os.path.join(os.path.dirname(__file__),
