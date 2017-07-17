c7251ec42314e83ffeb1a3269983513a3216ca31
sagar-kohli <kohli.sagar2@gmail.com>
2017-07-17 15:16:23 +0530

    check

--- src/damn_at/__init__.py
+++ src/damn_at/__init__.py
@@ -3,12 +3,6 @@ The DAMN AT module.
 """
 from __future__ import absolute_import
 import logging
-logger = logging.getLogger('damn_at')  # pylint: disable=C0103
-
-# Yapsy logs failed plugins as errors, but we don't like that.
-logging.getLogger('yapsy')._error = logging.getLogger('yapsy').error
-logging.getLogger('yapsy').error = logging.getLogger('yapsy').info
-
 
 from damn_at.serialization.generated.damn_types.ttypes import (
     TargetMimetype,
@@ -21,9 +15,15 @@ from damn_at.serialization.generated.damn_types.ttypes import (
     AssetDescription
 )
 
-from damn_at.analyzer import Analyzer  # pylint: disable=W0611
-from damn_at.metadatastore import MetaDataStore  # pylint: disable=W0611
-from damn_at.transcoder import Transcoder  # pylint: disable=W0611
+# from damn_at.analyzer import Analyzer  # pylint: disable=W0611
+# from damn_at.metadatastore import MetaDataStore  # pylint: disable=W0611
+# from damn_at.transcoder import Transcoder  # pylint: disable=W0611
+
+logger = logging.getLogger('damn_at')  # pylint: disable=C0103
+
+# Yapsy logs failed plugins as errors, but we don't like that.
+logging.getLogger('yapsy')._error = logging.getLogger('yapsy').error
+logging.getLogger('yapsy').error = logging.getLogger('yapsy').info
 
 _CMD_DESCRIPTION = r'''
  ___   _   __  __ _  _
@@ -35,4 +35,4 @@ _CMD_DESCRIPTION = r'''
 
 __all__ = ['TargetMimetype', 'TargetMimetypeOption', 'MetaDataType',
            'MetaDataValue', 'FileId', 'FileDescription', 'AssetId',
-           'AssetDescription', 'Analyzer', 'MetaDataStore', 'Transcoder']
+           'AssetDescription']
