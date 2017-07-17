f16994ffe68857aa6dc5995c49f56fd8bfef7365
sagar-kohli <kohli.sagar2@gmail.com>
2017-07-18 00:47:12 +0530

    undo

--- src/damn_at/__init__.py
+++ src/damn_at/__init__.py
@@ -3,6 +3,12 @@ The DAMN AT module.
 """
 from __future__ import absolute_import
 import logging
+logger = logging.getLogger('damn_at')  # pylint: disable=C0103
+
+# Yapsy logs failed plugins as errors, but we don't like that.
+logging.getLogger('yapsy')._error = logging.getLogger('yapsy').error
+logging.getLogger('yapsy').error = logging.getLogger('yapsy').info
+
 
 from damn_at.serialization.generated.damn_types.ttypes import (
     TargetMimetype,
@@ -18,12 +24,6 @@ from damn_at.serialization.generated.damn_types.ttypes import (
 from damn_at.analyzer import Analyzer  # pylint: disable=W0611
 from damn_at.metadatastore import MetaDataStore  # pylint: disable=W0611
 from damn_at.transcoder import Transcoder  # pylint: disable=W0611
-
-logger = logging.getLogger('damn_at')  # pylint: disable=C0103
-
-# Yapsy logs failed plugins as errors, but we don't like that.
-logging.getLogger('yapsy')._error = logging.getLogger('yapsy').error
-logging.getLogger('yapsy').error = logging.getLogger('yapsy').info
 
 _CMD_DESCRIPTION = r'''
  ___   _   __  __ _  _
